aeaf39426c2936a2bdb18e1c2f331b0f780619ae
Guillaume Scheibel <gscheibel@expedia.com>
2016-02-08 19:28:33 +0000

    Add first actors

--- src/main/java/AkkaJavaApp.java
+++ src/main/java/AkkaJavaApp.java
@@ -1,9 +1,27 @@
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 public class AkkaJavaApp {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("Expedia-java-actor-system");
 
-        System.out.println(actorSystem);
+        ActorRef actorRef = actorSystem.actorOf(Props.create(FirstActorJava.class), "first-java-actor");
+
+        actorRef.tell("Hello Expedia from Java app", ActorRef.noSender());
+
+        actorSystem.terminate();
+    }
+
+    public static class FirstActorJava extends UntypedActor {
+        LoggingAdapter logger = Logging.getLogger(getContext().system(), this);
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            logger.info("Message received:\n\t" + message);
+        }
     }
 }
