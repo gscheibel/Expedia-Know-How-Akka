e556f83a0b3335478a9d5dcf40022b1e9aec684e
Guillaume Scheibel <gscheibel@expedia.com>
2016-02-08 21:19:05 +0000

    First usage of Ask in Scala

--- src/main/scala/AkkaScalaApp.scala
+++ src/main/scala/AkkaScalaApp.scala
@@ -1,10 +1,20 @@
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{ActorRef, Props, Actor, ActorSystem}
 import akka.event.Logging
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.util.{Failure, Success}
 
 class FirstActorScala extends Actor {
   private val logger = Logging.getLogger(context.system, this)
 
   override def receive: Receive = {
+    case question: String if question.startsWith("Question:") =>
+      logger.info(s"Received a question:\n\t$question")
+      sender ! "42"
     case message: String => logger.info(s"Message received:\n\t$message")
   }
 }
@@ -12,9 +22,19 @@ class FirstActorScala extends Actor {
 object AkkaScalaApp {
   def main(args: Array[String]) {
     val system: ActorSystem = ActorSystem("Expedia-scala-actor-system")
+    val logger = Logging.getLogger(system, this)
+
+    implicit val defaultTimeout: Timeout = Timeout(500, TimeUnit.MILLISECONDS)
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
     val actorRef: ActorRef = system.actorOf(Props[FirstActorScala], "first-scala-actor")
 
     actorRef ! "Hello Expedia from Scala app"
+
+    val possibleAnswer = actorRef.ask("Question: What is the answer to the Ultimate Question of Life, The Universe, and Everything?")
+
+    val answer = Await.result(possibleAnswer, defaultTimeout.duration).asInstanceOf[String]
+    logger.info(s"The answer is '$answer")
 
     system.terminate()
   }
