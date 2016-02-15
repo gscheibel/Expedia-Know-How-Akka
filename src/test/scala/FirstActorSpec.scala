42df34ca78a70df6c4c87daba8419efa952001a4
Guillaume Scheibel <gscheibel@expedia.com>
2016-02-15 20:27:48 +0000

    Add test to cover question usecase

--- src/test/scala/FirstActorSpec.scala
+++ src/test/scala/FirstActorSpec.scala
@@ -1,13 +1,35 @@
-import akka.actor.ActorSystem
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
 import akka.testkit.TestKit
+import akka.util.Timeout
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
 
-class FirstActorSpec(_system: ActorSystem) extends TestKit(_system: ActorSystem) with Matchers with FunSpecLike with BeforeAndAfterAll {
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+class FirstActorSpec(_system: ActorSystem) extends TestKit(_system: ActorSystem) with Matchers with FunSpecLike with BeforeAndAfterAll with ScalaFutures {
+
+  implicit val defaultTimeout: Timeout = Timeout(500, TimeUnit.MILLISECONDS)
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   def this() = this(ActorSystem("testActorSystem"))
 
   override protected def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
     super.afterAll()
+  }
+
+  describe("An actor") {
+    it("must answer `42` when a question is asked") {
+      val actorRef = system.actorOf(Props[FirstActorScala])
+
+      val future: Future[String] = (actorRef ? "Question: This is a test question").mapTo[String]
+
+      whenReady(future) { response =>
+        response should be("42")
+      }
+    }
   }
 }
